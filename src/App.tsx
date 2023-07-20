54e4c5060bc4f5680d2f40b9da32ba3e49197830
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-20 17:20:21 +0600

    fix: app component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+function App() {
+  return <>
+    App
+  </>;
+}
+
+export default App;
