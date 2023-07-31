50d49973645437b1507a2d39146cf7078f3d5a83
Yersultan <ersultansansyzbaj@gmail.com>
2023-07-31 10:18:22 +0600

    fix: remove react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.scss";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
-// import 'sweetalert2/dist/sweetalert2.min.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { HomePage } from "./components/HomePage";
 
 function App() {
